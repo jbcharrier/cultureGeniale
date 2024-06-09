a3888eb02df0fc3cc676612557a58b7d6019a41a
jbcharrier <jbcharrier33@gmail.com>
2024-06-09 17:49:21 +0200

    install project and create structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContenuComponent } from './contenu/contenu.component';
+import { ProfilComponent } from './profil/profil.component';
+import { FormulesComponent } from './formules/formules.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContenuComponent,
+    ProfilComponent,
+    FormulesComponent,
+    InscriptionComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cultureGeniale';
